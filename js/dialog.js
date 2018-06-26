04ee3bd1e7fb5cbf12ce721a7015dbde89c5469c
DaironSib <daironsib@gmail.com>
2018-06-26 16:22:55 +0200

    module5-task2

--- js/dialog.js
+++ js/dialog.js
@@ -1,38 +1,40 @@
 var dialogHandle = document.querySelector(`.setup-user-pic`)
 var setup = document.querySelector(`.setup`)
 
-dialogHandle.addEventListener(`mousedown`, function(e){
-  e.preventDefault()
+(function () {
+  dialogHandle.addEventListener(`mousedown`, function(e){
+    e.preventDefault()
 
-  var startCoords = {
-    x: e.clientX,
-    y: e.clientY
-  }
-
-  function onMouseMove(moveEvt) {
-    moveEvt.preventDefault()
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+    var startCoords = {
+      x: e.clientX,
+      y: e.clientY
     }
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault()
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      }
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      }
+
+      setup.style.top = (setup.offsetTop - shift.y) + `px`
+      setup.style.left = (setup.offsetLeft - shift.x) + `px`
     }
 
-    setup.style.top = (setup.offsetTop - shift.y) + `px`
-    setup.style.left = (setup.offsetLeft - shift.x) + `px`
-  }
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault()
 
-  function onMouseUp(upEvt) {
-    upEvt.preventDefault()
+      document.removeEventListener(`mousemove`, onMouseMove)
+      document.removeEventListener(`mouseup`, onMouseUp)
+    }
 
-    document.removeEventListener(`mousemove`, onMouseMove)
-    document.removeEventListener(`mouseup`, onMouseUp)
-  }
-
-  document.addEventListener(`mousemove`, onMouseMove)
-  document.addEventListener(`mouseup`, onMouseUp)
-})
+    document.addEventListener(`mousemove`, onMouseMove)
+    document.addEventListener(`mouseup`, onMouseUp)
+  })
+})()
