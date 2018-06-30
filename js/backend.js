b8d3cd1460af515df7498c1f716368e035e83e6c
DaironSib <daironsib@gmail.com>
2018-06-30 14:33:21 +0200

    module6-task1 add promise

--- js/backend.js
+++ js/backend.js
@@ -2,29 +2,33 @@
 
 'use strict'
 
-function request(url, options, success, error) {
-  var xhr = new XMLHttpRequest()
-  xhr.responseType = 'json'
+function request(url, options) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest()
+    xhr.responseType = 'json'
+    xhr.timeout = options.timeout
+    xhr.open(options.method, url)
 
-  xhr.addEventListener(`load`, function () {
-    if (xhr.status === 200) {
-      success(xhr.response)
-    } else {
-      error('Статус ответа: ' + xhr.status + ' ' + xhr.statusText)
+    xhr.onload = function() {
+      if (this.status == 200) {
+        resolve(this.response)
+      } else {
+        var error = new Error(this.statusText)
+        error.code = this.status
+        reject(error)
+      }
     }
-  })
 
-  xhr.addEventListener('error', function () {
-    error('Произошла ошибка соединения')
-  })
+    xhr.onerror = function() {
+      reject(new Error(`Произошла ошибка соединения`))
+    }
 
-  xhr.addEventListener('timeout', function () {
-    error('Запрос не успел выполниться за ' + xhr.timeout + 'мс')
-  })
+    xhr.timeout = function() {
+      reject(new Error(`Запрос не успел выполниться за ${xhr.timeout} мс`))
+    }
 
-  xhr.timeout = options.timeout
-  xhr.open(options.method, url)
-  xhr.send()
+    xhr.send(options.data)
+  })
 }
 
 window.backend = (function () {
@@ -36,15 +40,24 @@ window.backend = (function () {
         timeout: 10000
       }
       request(URL, params, onLoad, onError)
+        .then(
+          response => onLoad(response),
+          error => onError(error)
+        )
     },
-    save: function (data, onSave, onError) {
+    save: function (data, onLoad, onError) {
       var URL = `https://js.dump.academy/code-and-magick`
 
       var params = {
         method: `POST`,
+        data: data,
         timeout: 10000
       }
       request(URL, params, onLoad, onError)
+        .then(
+          response => onLoad(response),
+          error => onError(error)
+        )
     }
   }
 })()
