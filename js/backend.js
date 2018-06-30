6dc4b5728016d9655cc9d67b9e35a7eae9c2ad9c
DaironSib <daironsib@gmail.com>
2018-06-30 14:05:06 +0200

    module6-task1 add request func

--- js/backend.js
+++ js/backend.js
@@ -2,57 +2,49 @@
 
 'use strict'
 
+function request(url, options, success, error) {
+  var xhr = new XMLHttpRequest()
+  xhr.responseType = 'json'
+
+  xhr.addEventListener(`load`, function () {
+    if (xhr.status === 200) {
+      success(xhr.response)
+    } else {
+      error('Статус ответа: ' + xhr.status + ' ' + xhr.statusText)
+    }
+  })
+
+  xhr.addEventListener('error', function () {
+    error('Произошла ошибка соединения')
+  })
+
+  xhr.addEventListener('timeout', function () {
+    error('Запрос не успел выполниться за ' + xhr.timeout + 'мс')
+  })
+
+  xhr.timeout = options.timeout
+  xhr.open(options.method, url)
+  xhr.send()
+}
+
 window.backend = (function () {
   return {
     load: function (onLoad, onError) {
       var URL = `https://js.dump.academy/code-and-magick/data`
-
-      var xhr = new XMLHttpRequest()
-      xhr.responseType = 'json'
-
-      xhr.addEventListener(`load`, function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response)
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText)
-        }
-      })
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения')
-      })
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс')
-      })
-
-      xhr.timeout = 10000
-
-      xhr.open(`GET`, URL)
-      xhr.send()
+      var params = {
+        method: `GET`,
+        timeout: 10000
+      }
+      request(URL, params, onLoad, onError)
     },
     save: function (data, onSave, onError) {
       var URL = `https://js.dump.academy/code-and-magick`
 
-      var xhr = new XMLHttpRequest()
-      xhr.responseType = 'json'
-
-      xhr.addEventListener('load', function () {
-        onSave()
-      })
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения')
-      })
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс')
-      })
-
-      xhr.timeout = 10000
-      
-      xhr.open('POST', URL)
-      xhr.send(data)
+      var params = {
+        method: `POST`,
+        timeout: 10000
+      }
+      request(URL, params, onLoad, onError)
     }
   }
 })()
